9e985ff8fcc7f19d47c946c1ee6e906a3d59580f
marex <noware22@gmail.com>
2012-04-13 16:44:05 -0400

    add main class stub and plugin.yml

---
+++ src/net/makeitonthe/wtf/WhatTheFish.java
@@ -0,0 +1,28 @@
+/**
+ * Bukkit plugin for random and fun new fishing loots.
+ * authors: Marex and Zonta.
+ * 
+ * contact us at : plugins@makeitonthe.net
+ * 
+ * Copyright (C) 2012 Marex and Zonta
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.makeitonthe.wtf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WhatTheFish extends JavaPlugin {
+
+}
